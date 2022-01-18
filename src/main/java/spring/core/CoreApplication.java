20448f74eca04a1b6371283b5152f0a620518b20
leeyoungchun <dldydcns123@gmail.com>
2022-01-18 23:20:42 +0900

    first commit

---
+++ src/main/java/spring/core/CoreApplication.java
@@ -0,0 +1,14 @@
+package spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@SpringBootApplication
+public class CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
