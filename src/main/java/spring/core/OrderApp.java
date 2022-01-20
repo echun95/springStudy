f312dfa7ec7f4799622028f7c265edfc7be016a9
leeyoungchun <dldydcns123@gmail.com>
2022-01-20 23:31:25 +0900

    Appconfig 스프링으로 전환 및 빈 등록, 등록 된 빈으로 DI 및 테스트

--- src/main/java/spring/core/OrderApp.java
+++ src/main/java/spring/core/OrderApp.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
@@ -11,9 +13,13 @@ import spring.core.order.OrderServiceImpl;
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
