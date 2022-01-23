8f9ead65b5b170156c97da5a07869b16e6e98d2c
leeyoungchun <dldydcns123@gmail.com>
2022-01-23 18:09:22 +0900

    @Configuration 과 Singleton 테스트 및 알아보기

---
+++ src/test/java/spring/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,26 @@
+package spring.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AppConfig;
+import spring.core.member.MemberRepository;
+import spring.core.member.MemberServiceImpl;
+import spring.core.order.OrderServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        assertThat(memberRepository).isEqualTo(memberService.getMemberRepository());
+        assertThat(memberService.getMemberRepository()).isEqualTo(orderService.getMemberRepository());
+    }
+}
