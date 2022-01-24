423ce3fa528cc7aa764719cc2db9b1fdab857353
leeyoungchun <dldydcns123@gmail.com>
2022-01-24 23:27:54 +0900

    @ComponentScan 탐색 위치 지정(basePackages, basePackageClasses) 실습 및 기본 스캔 대상 확인(Controller, Service, Repository, Component, Configuration)

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "spring.core",
+        basePackageClasses = AutoAppConfig.class, //basePackages 지정안하면 default로 현재 패키지가 시작 위치
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
